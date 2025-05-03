13b5a4251aea74fae2c088ab70c1e486ba488830
MickEnev <mickenev@gmail.com>
2025-05-03 07:45:02 -0500

    chip8 class partial implementation

--- include/headers/chip8.h
+++ include/headers/chip8.h
@@ -1,10 +1,41 @@
 #ifndef CHIP8_H
 #define CHIP8_H
 
+#include "memory.h"
+#include "stack.h"
+#include <cstdint>
+
 class Chip8 {
     public:
+    Chip8();
+    void loadROM(const char* filename);
+    void cycle();           // Fetch, decode, execute
+    void updateTimers();    // Called at 60Hz
 
     private:
-}
+    Memory _memory;
+    Stack _stack;
+
+    // Registers
+    uint8_t _V[16];  // V0 to VF
+
+    // Index register and program counter
+    uint16_t _I;
+    uint16_t _PC;
+
+    // Timers
+    uint8_t _delayTimer;
+    uint8_t _soundTimer;
+
+    // Display buffer
+    uint8_t _video[64 * 32];
+
+    // input
+    uint8_t _keypad[16];
+
+    // Fetch, decode, execute helpers
+    void executeOpcode(uint16_t opcode);
+    
+};
 
 #endif
--- src/chip8.cpp
+++ src/chip8.cpp
@@ -1,1 +1,31 @@
 #include "chip8.h"
+#include <fstream>
+#include <stdexcept>
+
+Chip8::Chip8() : _I(0), _PC(0), _delayTimer(0), _soundTimer(0)
+    {
+        // Clear memory
+        std::fill(std::begin(_V), std::end(_V), 0);
+        std::fill(std::begin(_video), std::end(_video), 0);
+        std::fill(std::begin(_keypad), std::end(_keypad), 0);
+
+        _memory.loadFontSet();
+    }
+
+void Chip8::loadROM(const char* filename) {
+    // TODO: Figure out how to load a rom... LOL
+}
+
+void Chip8::cycle() {
+    // Fetch
+    uint16_t opcode = (_memory.read(_PC) << 8 | _memory.read(_PC + 1));
+    _PC += 2;
+
+    // Decode and execute
+    executeOpcode(opcode);
+}
+
+void Chip8::updateTimers() {
+    if (_delayTimer > 0) --_delayTimer;
+    if (_soundTimer > 0) --_soundTimer;
+}
